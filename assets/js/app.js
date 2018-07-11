8f384df3d89a3159bb652a756cb5a292b0a9480b
MeepyMay <meepymay@gmail.com>
2018-07-10 21:04:09 -0400

    removed one multiplied error and cleaned up file locations and code

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,37 +1,30 @@
 //Firebase
 var config = {
-    apiKey: "AIzaSyCn4XC2OW3MeQMqQ8mGhgG0AL1ncDmjGOY",
-    authDomain: "cupcake-brigade.firebaseapp.com",
-    databaseURL: "https://cupcake-brigade.firebaseio.com",
-    projectId: "cupcake-brigade",
-    storageBucket: "cupcake-brigade.appspot.com",
-    messagingSenderId: "1092615531812"
-  };
-  firebase.initializeApp(config);
-  var database = firebase.database();
-
-
-
-$(document).ready(function(){
-
-login();
-
+	apiKey: "AIzaSyCn4XC2OW3MeQMqQ8mGhgG0AL1ncDmjGOY",
+	authDomain: "cupcake-brigade.firebaseapp.com",
+	databaseURL: "https://cupcake-brigade.firebaseio.com",
+	projectId: "cupcake-brigade",
+	storageBucket: "cupcake-brigade.appspot.com",
+	messagingSenderId: "1092615531812"
+};
+firebase.initializeApp(config);
+var database = firebase.database();
+$(document).ready(function() {
+	login();
 });
 
-function login(){
-    function newLogin(user){
-        if (user)
-        {
-            testAlert(user);
-        } else {
-            var provider = new firebase.auth.GoogleAuthProvider();
-            firebase.auth().signInWithRedirect(provider);
-        }
-    }
-
-    firebase.auth().onAuthStateChanged(newLogin);
+function login() {
+	function newLogin(user) {
+		if (user) {
+			testAlert(user);
+		} else {
+			var provider = new firebase.auth.GoogleAuthProvider();
+			firebase.auth().signInWithRedirect(provider);
+		}
+	}
+	firebase.auth().onAuthStateChanged(newLogin);
 }
 
-function testAlert(user){
-    console.log("hello " + user.displayName);
+function testAlert(user) {
+	console.log("hello " + user.displayName);
 }

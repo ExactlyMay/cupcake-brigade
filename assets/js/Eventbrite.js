208fccb920a54008c2dc2d42c278345318363c19
May Burgos <meepymay@gmail.com>
2018-07-12 18:24:51 -0400

    Update Eventbrite.js
    fixing a few bugs

--- assets/js/Eventbrite.js
+++ assets/js/Eventbrite.js
@@ -2368,7 +2368,7 @@ $("#submitBtn").on("click", function (event) {
                                 
                                 var text1 = "<a href="+u+"> Click Here To Buy Tickets  </a>"
                                
-                                $("#Eventslist > tbody").append("<tr><td>" + n + "</td><td>" + response2.address.address_1 + "</td><td id='min'>" +response2.address.city + "</td><td id='min'>" + text1 + "</td><td id='min'>" + d + "</td><td id='min'>"+response2.address.country+"</td></tr>");
+                                $("#Eventslist > tbody").prepend("<tr><td>" + n + "</td><td>" + response2.address.address_1 + "</td><td id='min'>" +response2.address.city + "</td><td id='min'>" + text1 + "</td><td id='min'>" + d + "</td><td id='min'>"+response2.address.country+"</td></tr>");
 
                                 
                                 }
@@ -2427,7 +2427,9 @@ $("#speechbtn").on("click", function (event) {
 });
 
 $("#clearresult").on("click",function(){
-                                        $('#Eventslist tbody').remove();
+$('#Eventslist tbody tr').remove();
+$("#clearresult").hide();
+$("#Eventslist").hide();
 
 })
 
